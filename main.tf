4f1ff5eae246230481f41f93e8da91546087dc24
KowshikNaga <150836662+KowshikNaga@users.noreply.github.com>
2024-10-03 21:06:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "1.9.7"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
+  access_key = "AKIA3ISBVXA6KCV7BFWD"
+  secret_key = "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"
 
-variable "aws_access_key" {
-  description = "AKIA3ISBVXA6KCV7BFWD"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "1S1sZSiMsPWQxo4TFJ2KJh3FTSopbCThSCh+RWqY"
-  type        = string
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d1622042e957c247 "  # Replace with a valid AMI ID
+  ami           = "ami-0d1622042e957c247"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
 }
