6b9d6d12b9e8928d3e20543d1ba0c0b551dd2a27
KowshikNaga <150836662+KowshikNaga@users.noreply.github.com>
2024-10-03 21:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# Declare the variables for AWS credentials
+variable "aws_access_key" {
+  description = "AKIA3ISBVXA6KCV7BFWD"
+  type        = string
 }
 
+variable "aws_secret_key" {
+  description = "1S1sZSiMsPWQxo4TFJ2KJh3FTSopbCThSCh+RWqY"
+  type        = string
+}
+
+# Use the variables in the provider block
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3ISBVXA6KCV7BFWD"
-  secret_key = "1S1sZSiMsPWQxo4TFJ2KJh3FTSopbCThSCh+RWqY"
-
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0d1622042e957c247"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
 }
+
