def84514c9fb174803d3a9d4c0a26d0029c002f4
KowshikNaga <150836662+KowshikNaga@users.noreply.github.com>
2024-10-04 10:26:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Declare the variables for AWS credentials
 variable "aws_access_key" {
-  description = "AKIA3ISBVXA6KCV7BFWD"
+  description = "AKIA3ISBVXA6E7KG4W3P"
   type        = string
 }
 
 variable "aws_secret_key" {
-  description = "1S1sZSiMsPWQxo4TFJ2KJh3FTSopbCThSCh+RWqY"
+  description = "0++Ms+p0cQrBAao6vfCK4CMOwPUR6v6/paa4ol8T"
   type        = string
 }
 
@@ -16,7 +16,7 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "instance" {
   ami           = "ami-0d1622042e957c247"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
 }
